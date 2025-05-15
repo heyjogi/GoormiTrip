46c24a953584a14042f6dcd7fe95f5f438b4901d
chohyejung <hj1021007@gmail.com>
2025-05-15 14:53:24 +0900

    initial commit

---
+++ src/main/java/com/goormitrip/goormitrip/GoormitripApplication.java
@@ -0,0 +1,13 @@
+package com.goormitrip.goormitrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormitripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormitripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormitrip/goormitrip/GoormitripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormitrip.goormitrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormitripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
