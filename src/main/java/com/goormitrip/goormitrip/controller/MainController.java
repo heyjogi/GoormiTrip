3c91023ee0698752a30e0769132a5bafaf6991ab
chohyejung <hj1021007@gmail.com>
2025-05-19 09:23:18 +0900

    email login 인식

--- src/main/java/com/goormitrip/goormitrip/controller/MainController.java
+++ src/main/java/com/goormitrip/goormitrip/controller/MainController.java
@@ -1,41 +1,20 @@
 package com.goormitrip.goormitrip.controller;
 
-import com.goormitrip.goormitrip.domain.User;
-import com.goormitrip.goormitrip.dto.SignupRequestDto;
-import com.goormitrip.goormitrip.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
 
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public MainController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-//        this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
-//    }
-
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        String email = principal.getName();
+        model.addAttribute("email", email);
         return "main";
     }
 
-//    @GetMapping("/signup")
-//    public String signupForm() {
-//        return "signup";
-//    }
-//
-//    @PostMapping("/signup")
-//    public String processSignup(SignupRequestDto signupRequestDto) {
-//        userRepository.save(signupRequestDto.toUser(passwordEncoder));
-//        return "redirect:/login";
-//    }
 
 }
