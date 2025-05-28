1730a90865888a8f0e844987dfe29933d3bbb764
Changyeol Choe <duf325@naver.com>
2025-05-28 19:16:11 +0900

    chore: LB 헬스체크 용도의 API 작성

---
+++ src/main/java/com/goormitrip/goormitrip/global/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.goormitrip.goormitrip.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "ok";
+	}
+}
