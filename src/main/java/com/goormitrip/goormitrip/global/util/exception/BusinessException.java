5aff9d74af01267d5560e5920380b580e4857e70
Changyeol Choe <duf325@naver.com>
2025-06-03 16:15:18 +0900

    perf: 동적 파라미터 포함 메시지를 위한 공통 응답객체의 오버로딩 생성자 추가

--- src/main/java/com/goormitrip/goormitrip/global/util/exception/BusinessException.java
+++ src/main/java/com/goormitrip/goormitrip/global/util/exception/BusinessException.java
@@ -5,9 +5,15 @@ import lombok.Getter;
 @Getter
 public abstract class BusinessException extends RuntimeException {
 	private final ErrorCode errorCode;
+	private final String detail;
 
-	protected BusinessException(final ErrorCode errorCode) {
-		super(errorCode.getMessage());
-		this.errorCode = errorCode;
+	protected BusinessException(ErrorCode ec) {
+		this(ec, null);
+	}
+
+	protected BusinessException(ErrorCode ec, String detail) {
+		super(detail == null ? ec.getFormattedMessage() : detail);
+		this.errorCode = ec;
+		this.detail = detail;
 	}
 }
--- src/main/java/com/goormitrip/goormitrip/global/util/response/ApiResponse.java
+++ src/main/java/com/goormitrip/goormitrip/global/util/response/ApiResponse.java
@@ -3,6 +3,7 @@ package com.goormitrip.goormitrip.global.util.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.goormitrip.goormitrip.global.util.exception.BusinessException;
 import com.goormitrip.goormitrip.global.util.exception.ErrorCode;
 
 public record ApiResponse<T>(
@@ -29,5 +30,13 @@ public record ApiResponse<T>(
 	public static <T> ResponseEntity<ApiResponse<T>> error(final ErrorCode errorCode) {
 		return ResponseEntity.status(errorCode.getStatus())
 			.body(fail(errorCode.getFormattedMessage(), errorCode.getStatus()));
+	}
+
+	public static <T> ResponseEntity<ApiResponse<T>> error(final BusinessException ex) {
+		final String message = ex.getDetail() != null
+			? ex.getDetail()
+			: ex.getErrorCode().getFormattedMessage();
+		return ResponseEntity.status(ex.getErrorCode().getStatus())
+			.body(fail(message, ex.getErrorCode().getStatus()));
 	}
 }
