e7d942723b16f251eff65156933dd536d865e2d6
Changyeol Choe <duf325@naver.com>
2025-06-09 21:45:06 +0900

    ################ # <type>(<scope>): <subject> 의 형식으로 제목을 아래 공백줄에 작성 # scope는 선택사항 # 제목은 50자 이내, 변경사항 명확히 작성, 끝에 마침표 금지 feat: 로그인 성공 시 토큰 응답 반환 처리 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(body)을 아랫줄에 작성
    
    ################
    # 꼬리말(footer)을 아랫줄에 작성
    
    ################ Minor 버전이 올라가는 경우
    # ✨ feat: 새로운 기능의 추가
    ################ Patch 버전이 올라가는 경우
    # 🐛 fix: 버그 수정
    # 📝 docs: 문서 수정
    # 💄 style: 스타일 관련 수정
    # ♻️ refactor: 코드 리팩토링
    # ⚡️ perf: 성능 개선
    # ⏪️ revert: 이전 커밋으로 되돌리는 경우
    ################ 버전에 영향을 안끼치는 경우
    # ✅ test: 테스트 코드
    # 🔧 chore: 빌드 관련, 패키지 매니저 수정
    # 👷 ci: ci 설정 및 스크립트 변경
    # 📦 build: 빌드 시스템 또는 외부 종속성에 영향을 미치는 변경 사항
    ################

---
+++ src/main/java/com/goormitrip/goormitrip/oauth/security/JwtIssuerSuccessHandler.java
@@ -0,0 +1,53 @@
+package com.goormitrip.goormitrip.oauth.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goormitrip.goormitrip.global.security.CustomUserDetails;
+import com.goormitrip.goormitrip.global.security.JwtTokenProvider;
+import com.goormitrip.goormitrip.global.security.TokenResponse;
+import com.goormitrip.goormitrip.global.util.response.ApiResponse;
+import com.goormitrip.goormitrip.oauth.RefreshTokenService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtIssuerSuccessHandler implements AuthenticationSuccessHandler {
+
+	private final JwtTokenProvider tokenProvider;
+	private final RefreshTokenService refreshService;
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void onAuthenticationSuccess(
+		HttpServletRequest req,
+		HttpServletResponse res,
+		Authentication auth
+	) throws IOException {
+
+		final CustomOAuth2User oAuth2User = (CustomOAuth2User) auth.getPrincipal();
+		final CustomUserDetails userDetails = oAuth2User.getUserDetails();
+
+		String access = tokenProvider.createAccessToken(userDetails.getId(), userDetails.getRole());
+		String refresh = tokenProvider.createRefreshToken(userDetails.getId());
+		refreshService.store(userDetails.toUserEntityForTokenStorage(), refresh);
+
+		TokenResponse tokenResponse = new TokenResponse(access, refresh);
+		ApiResponse<?> apiResponse = createSuccessResponse(tokenResponse);
+
+		res.setContentType("application/json");
+		res.setCharacterEncoding("UTF-8");
+		res.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+	}
+
+	private <T> ApiResponse<T> createSuccessResponse(T data) {
+		return ApiResponse.ok(data).getBody();
+	}
+}
