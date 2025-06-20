c6996a36a25d1672b4da6e9ac27a23bda2eb1248
chohyejung <hj1021007@gmail.com>
2025-06-20 14:25:56 +0900

    Payment controller 추가

--- src/main/java/com/goormitrip/goormitrip/payment/controller/PaymentController.java
+++ src/main/java/com/goormitrip/goormitrip/payment/controller/PaymentController.java
@@ -1,4 +1,27 @@
 package com.goormitrip.goormitrip.payment.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.goormitrip.goormitrip.global.util.response.ApiResponse;
+import com.goormitrip.goormitrip.payment.dto.ConfirmPaymentRequest;
+import com.goormitrip.goormitrip.payment.dto.ConfirmPaymentResponse;
+import com.goormitrip.goormitrip.payment.service.PaymentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
+
+	private final PaymentService paymentService;
+
+	@PostMapping("/confirm")
+	public ResponseEntity<ApiResponse<ConfirmPaymentResponse>> confirm(@RequestBody ConfirmPaymentRequest request) {
+		return ApiResponse.ok(paymentService.confirmPayment(request));
+	}
 }
