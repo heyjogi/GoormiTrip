54c27fb81e96cd87abc935844497804576b1d422
Changyeol Choe <duf325@naver.com>
2025-06-21 23:30:32 +0900

    feat: 소셜로그인 유저 로그인 시도 예외처리 로직을 로그인 필터단으로 이동

--- src/main/java/com/goormitrip/goormitrip/global/config/SecurityConfig.java
+++ src/main/java/com/goormitrip/goormitrip/global/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,12 +50,8 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(passwordEncoder());
-
-		return authProvider;
+	public AuthenticationProvider authenticationProvider() {
+		return new CustomDaoAuthenticationProvider(userDetailsService, passwordEncoder());
 	}
 
 	@Bean
