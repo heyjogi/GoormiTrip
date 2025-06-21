e3a6f59d5d4a6463be7483b8ae8cf8672138e87f
Changyeol Choe <duf325@naver.com>
2025-06-21 19:16:01 +0900

    feat: 사용자 프로필과 엔티티 조인 쿼리 추가

--- src/main/java/com/goormitrip/goormitrip/user/repository/UserRepository.java
+++ src/main/java/com/goormitrip/goormitrip/user/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.goormitrip.goormitrip.user.repository;
 
-import com.goormitrip.goormitrip.user.domain.UserEntity;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.goormitrip.goormitrip.user.domain.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
 
     Optional<UserEntity> findByEmail(String email);
+
+    @EntityGraph(attributePaths = "profile")
+    Optional<UserEntity> findById(Long id);
 }
