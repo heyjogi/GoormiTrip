7275339a2aed174cde4538e32e9b6a6d6c325632
Changyeol Choe <duf325@naver.com>
2025-06-22 10:30:57 +0900

    feat: 비밀번호 변경 서비스 설계 및 구현

--- src/main/java/com/goormitrip/goormitrip/user/exception/UserError.java
+++ src/main/java/com/goormitrip/goormitrip/user/exception/UserError.java
@@ -19,7 +19,9 @@ public enum UserError implements ErrorCode {
 	PHONE_VERIFICATION_FAILED(HttpStatus.CONFLICT, "USER-007", "인증에 실패하였습니다."),
 	LOGIN_FAILED(HttpStatus.CONFLICT, "USER-008", "이메일 또는 비밀번호가 잘못되었습니다."),
 	SOCIAL_LOGIN_USER_CANNOT_LOGIN(HttpStatus.CONFLICT, "USER-009", "소셜 로그인 사용자는 일반 로그인이 불가능합니다."),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-010", "사용자를 찾을 수 없습니다.");
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-010", "사용자를 찾을 수 없습니다."),
+	INVALID_RESET_TOKEN(HttpStatus.NOT_FOUND, "USER-011", "유효하지 않은 비밀번호 재설정 토큰입니다."),
+	MAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "USER-012", "메일 전송에 실패했습니다.");
 
 	private final HttpStatus status;
 	private final String code;
