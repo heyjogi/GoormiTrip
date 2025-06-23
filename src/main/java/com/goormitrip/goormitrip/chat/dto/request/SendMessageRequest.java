d01d785ce6cd38e89ad42a28eb0c7f2cbb597bc2
Changyeol Choe <duf325@naver.com>
2025-06-23 18:10:01 +0900

    chore: 명세에 맞게 채팅 요청 필드명 변경

--- src/main/java/com/goormitrip/goormitrip/chat/dto/request/SendMessageRequest.java
+++ src/main/java/com/goormitrip/goormitrip/chat/dto/request/SendMessageRequest.java
@@ -2,4 +2,5 @@ package com.goormitrip.goormitrip.chat.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record SendMessageRequest(@NotBlank String content) { }
+public record SendMessageRequest(@NotBlank String message) {
+}
