74f116a0ab666dccb69bfb23297b647e6b15e950
Changyeol Choe <duf325@naver.com>
2025-06-23 17:17:23 +0900

    feat: 관리자 선택을 위한 유저 레포지토리 메서드 추가

--- src/main/java/com/goormitrip/goormitrip/user/repository/UserRepository.java
+++ src/main/java/com/goormitrip/goormitrip/user/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.goormitrip.goormitrip.user.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.goormitrip.goormitrip.user.domain.UserEntity;
+import com.goormitrip.goormitrip.user.domain.UserRole;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
@@ -14,4 +16,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     @EntityGraph(attributePaths = "profile")
     Optional<UserEntity> findFetchProfileById(Long id);
+
+    List<UserEntity> findAllByRole(UserRole role);
+
+    long countByRole(UserRole role);
 }
