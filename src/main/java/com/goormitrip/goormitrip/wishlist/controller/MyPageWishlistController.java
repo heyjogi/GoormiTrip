dc3667027777fc72dc66ff642981c2ae6975bd85
chohyejung <hj1021007@gmail.com>
2025-07-02 11:26:17 +0900

    찜한 상품 조회 공통api response 반영

--- src/main/java/com/goormitrip/goormitrip/wishlist/controller/MyPageWishlistController.java
+++ src/main/java/com/goormitrip/goormitrip/wishlist/controller/MyPageWishlistController.java
@@ -2,13 +2,14 @@ package com.goormitrip.goormitrip.wishlist.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.goormitrip.goormitrip.global.util.response.ApiResponse;
 import com.goormitrip.goormitrip.user.domain.UserEntity;
-import com.goormitrip.goormitrip.wishlist.domain.Wishlist;
 import com.goormitrip.goormitrip.wishlist.dto.WishlistResponse;
 import com.goormitrip.goormitrip.wishlist.service.WishlistService;
 
@@ -23,18 +24,10 @@ public class MyPageWishlistController {
 	}
 
 	@GetMapping("/wishlist")
-	public List<WishlistResponse> getMyWishlist(
+	public ResponseEntity<ApiResponse<List<WishlistResponse>>> getMyWishlist(
 		@AuthenticationPrincipal UserEntity user) {
-		List<Wishlist> wishlists = wishlistService.getWishlist(user);
+		List<WishlistResponse> response = wishlistService.getWishlist(user);
+		return ApiResponse.ok(response);
 
-		return wishlists.stream()
-			.map(wishlist -> WishlistResponse.builder()
-				.id(wishlist.getId())
-				.productId(wishlist.getProduct().getId())
-				.productTitle(wishlist.getProduct().getTitle())
-				.thumbnail(wishlist.getProduct().getThumbnail())
-				.build())
-			.toList();
 	}
-
 }
